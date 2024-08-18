e3a404c1fe22a13c2dbda2cbeaea13698310cf86
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-08-18 20:40:56 -0300

    BulletSpawner & Bullet

--- GMTK 2024/Assets/_Game/_Scripts/Entities/BulletSpawner/BulletCollision.cs
+++ GMTK 2024/Assets/_Game/_Scripts/Entities/BulletSpawner/BulletCollision.cs
@@ -6,13 +6,13 @@ public class BulletCollision : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 9)
+        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 6)
             Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 9)
+        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 6)
             Destroy(gameObject);
     }
 }
