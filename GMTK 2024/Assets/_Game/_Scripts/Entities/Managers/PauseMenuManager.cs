832037b98352138c42a1ab88d6d9988b63c835f6
Reimyel <caiquegsambrana@gmail.com>
2024-08-20 10:31:23 -0300

    pauses

--- GMTK 2024/Assets/_Game/_Scripts/Entities/Managers/PauseMenuManager.cs
+++ GMTK 2024/Assets/_Game/_Scripts/Entities/Managers/PauseMenuManager.cs
@@ -1,23 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using 
 
 public class PauseMenuManager : MonoBehaviour
 {
-    private GameObject _pausePanel;
+    [Header("Referências:")]
+    [SerializeField] private GameObject _pausePanel;
+    private bool _paused = false;
 
-    private void Update()
+    public void Update()
     {
-
+        if (_pausePanel != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!_paused)
+                {
+                    Debug.Log("Pausou");
+                    Pause();
+                }
+                else
+                {
+                    Debug.Log("Despausou");
+                    Continue();
+                }
+            }
+        }
     }
 
     public void Pause()
     {
         _pausePanel.SetActive(true);
+        _paused = true;
+
+        
     }
 
     public void Continue()
     {
         _pausePanel.SetActive(false);
+        _paused = false;
     }
 }
