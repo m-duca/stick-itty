f7a0af8b1f32272036bd0cdaaf8690c786e3a447
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-08-25 17:00:42 -0300

    Fix BaseMoveChanger

--- GMTK 2024/Assets/_Game/_Scripts/Entities/BulletSpawner/BulletCollision.cs
+++ GMTK 2024/Assets/_Game/_Scripts/Entities/BulletSpawner/BulletCollision.cs
@@ -6,13 +6,13 @@ public class BulletCollision : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 6)
+        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 6 && collision.gameObject.layer != 24)
             Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 6)
+        if (collision.gameObject.layer != 3 && collision.gameObject.layer != 6 && collision.gameObject.layer != 24) 
             Destroy(gameObject);
     }
 }
